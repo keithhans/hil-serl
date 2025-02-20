85099fbd1668d8f666bce130d23f3a9e57b8970f
keith <keithhans@gmail.com>
2025-02-20 22:17:59 +0800

    disable second view

--- franka_sim/franka_sim/utils/viewer_utils.py
+++ franka_sim/franka_sim/utils/viewer_utils.py
@@ -28,27 +28,27 @@ class DualMujocoViewer:
             show_right_ui=False,
             key_callback=self.key_callback
         )
-        self.viewer_2 = mujoco.viewer.launch_passive(
-            self.model,
-            self.data,
-            show_left_ui=False,
-            show_right_ui=False,
-            key_callback=self.key_callback
-        )
+        # self.viewer_2 = mujoco.viewer.launch_passive(
+        #     self.model,
+        #     self.data,
+        #     show_left_ui=False,
+        #     show_right_ui=False,
+        #     key_callback=self.key_callback
+        # )
 
     def is_running(self):
-        return self.viewer_1.is_running() and self.viewer_2.is_running()
+        return self.viewer_1.is_running()# and self.viewer_2.is_running()
 
     def sync(self):
         if self.viewer_1:
             self.viewer_1.sync()
-        if self.viewer_2:
-            self.viewer_2.sync()
+        # if self.viewer_2:
+        #     self.viewer_2.sync()
 
     def close(self):
         import glfw
         if self.viewer_1:
             self.viewer_1.close()
-        if self.viewer_2:
-            self.viewer_2.close()
+        # if self.viewer_2:
+        #     self.viewer_2.close()
         glfw.terminate()
